12749d1956f9a96bfae7409c0334f11b24ae727b
Hitoshi Honma <homma_hitoshi_gn@cyberagent.co.jp>
2019-01-21 14:05:38 +0900

    Add js code generated by grpc-vuex

--- client/src/App.vue
+++ client/src/App.vue
@@ -8,9 +8,7 @@
 </template>
 
 <script>
-import moment from 'moment'
-import { GreeterPromiseClient } from './grpc/helloworld_grpc_web_pb'
-import { HelloRequest } from './grpc/helloworld_pb'
+import { sayHello } from './grpc/bundle'
 
 export default {
   name: 'App',
@@ -21,18 +19,15 @@ export default {
   },
   methods: {
     submit () {
-      const req = new HelloRequest()
-      const users = [0].map(()=>{
-        const r = new HelloRequest.User()
-        r.setName('puyo')
-        r.setAge(999)
-        r.setChildrenList(['uuu'])
-        return r
+      return sayHello({
+        users: [
+          {
+            name: 'puyopuyo',
+            age: 999999,
+            children: ['uuuuuu'],
+          },
+        ],
       })
-      req.setUsersList(users)
-      const greeter = new GreeterPromiseClient('http://localhost:8080')
-      return greeter
-        .sayHello(req, {deadline: moment().add(5, 'seconds').format('x')})
         .then((res)=>console.log(res.toObject()))
         .catch((err)=>console.error(err))
     },
